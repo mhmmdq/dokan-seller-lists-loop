258a288abcf86f39010ae16c7ef1988f122fde6a
mhmmdq <mhmmdq@mhmmdq.ir>
2022-03-14 18:22:13 +0330

    first commit

---
+++ dokan-seller.lists-loop.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Plugin Name: اصلاح لیست فروشندگان در صفحه برند ها
+Description: افزودن نام شهر و استان به لیست فروشندگان دکان
+Author: Mhmmdq
+Version: 1.0.0
+Author URI: https://github.com/mhmmdq
+*/
+
+
+function dokan_seller_listing_footer_content_add_city_state( $seller )
+{
+     $store_id = $seller->ID;
+	 $vendor = dokan()->vendor->get( $seller->ID );;
+	 $store_info = dokan_get_store_info( $store_id );
+    
+	echo "<h3> <a href='{$vendor->get_shop_url()}'> {$store_info['store_name']} </a> </h3>";
+}
+
+add_action( 'dokan_seller_listing_footer_content' , 'dokan_seller_listing_footer_content_add_city_state');
